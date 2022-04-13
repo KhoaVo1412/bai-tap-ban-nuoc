fc1b88e6bdf1801d27d127226b1f91a46e38a20d
Khoa Võ <khoanguyen.1911020@gmail.com>
2022-04-13 07:18:01 +0700

    frist commit

---
+++ BaiTuan3/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿
+namespace BaiTuan3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grb1 = new System.Windows.Forms.GroupBox();
+            this.lblThanhtien = new System.Windows.Forms.Label();
+            this.lblHoadon = new System.Windows.Forms.Label();
+            this.lblSoluong = new System.Windows.Forms.Label();
+            this.form2 = new System.Windows.Forms.GroupBox();
+            this.lblTrungbinhLanban = new System.Windows.Forms.Label();
+            this.lblTongtien = new System.Windows.Forms.Label();
+            this.lblTongsl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtThanhtien = new System.Windows.Forms.Label();
+            this.txtTongtien = new System.Windows.Forms.Label();
+            this.btnBanmoi = new System.Windows.Forms.Button();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.radCfda = new System.Windows.Forms.RadioButton();
+            this.radCfsua = new System.Windows.Forms.RadioButton();
+            this.radBia = new System.Windows.Forms.RadioButton();
+            this.radRebbull = new System.Windows.Forms.RadioButton();
+            this.ckbMuave = new System.Windows.Forms.CheckBox();
+            this.txtTb = new System.Windows.Forms.Label();
+            this.txtSl = new System.Windows.Forms.Label();
+            this.txtHoadon = new System.Windows.Forms.Label();
+            this.btnXemthongtin = new System.Windows.Forms.Button();
+            this.grb1.SuspendLayout();
+            this.form2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grb1
+            // 
+            this.grb1.Controls.Add(this.ckbMuave);
+            this.grb1.Controls.Add(this.radRebbull);
+            this.grb1.Controls.Add(this.radBia);
+            this.grb1.Controls.Add(this.radCfsua);
+            this.grb1.Controls.Add(this.radCfda);
+            this.grb1.Controls.Add(this.btnThoat);
+            this.grb1.Controls.Add(this.btnTinh);
+            this.grb1.Controls.Add(this.btnBanmoi);
+            this.grb1.Controls.Add(this.txtHoadon);
+            this.grb1.Controls.Add(this.txtThanhtien);
+            this.grb1.Controls.Add(this.textBox1);
+            this.grb1.Controls.Add(this.lblThanhtien);
+            this.grb1.Controls.Add(this.lblHoadon);
+            this.grb1.Controls.Add(this.lblSoluong);
+            this.grb1.Location = new System.Drawing.Point(0, 0);
+            this.grb1.Name = "grb1";
+            this.grb1.Size = new System.Drawing.Size(800, 214);
+            this.grb1.TabIndex = 0;
+            this.grb1.TabStop = false;
+            this.grb1.Text = "Thong tin nhap";
+            // 
+            // lblThanhtien
+            // 
+            this.lblThanhtien.Location = new System.Drawing.Point(52, 128);
+            this.lblThanhtien.Name = "lblThanhtien";
+            this.lblThanhtien.Size = new System.Drawing.Size(133, 36);
+            this.lblThanhtien.TabIndex = 0;
+            this.lblThanhtien.Text = "Thanh Tien";
+            // 
+            // lblHoadon
+            // 
+            this.lblHoadon.Location = new System.Drawing.Point(52, 78);
+            this.lblHoadon.Name = "lblHoadon";
+            this.lblHoadon.Size = new System.Drawing.Size(88, 38);
+            this.lblHoadon.TabIndex = 0;
+            this.lblHoadon.Text = "Hoa Don";
+            // 
+            // lblSoluong
+            // 
+            this.lblSoluong.Location = new System.Drawing.Point(52, 40);
+            this.lblSoluong.Name = "lblSoluong";
+            this.lblSoluong.Size = new System.Drawing.Size(88, 38);
+            this.lblSoluong.TabIndex = 0;
+            this.lblSoluong.Text = "So Luong";
+            // 
+            // form2
+            // 
+            this.form2.Controls.Add(this.lblTrungbinhLanban);
+            this.form2.Controls.Add(this.lblTongtien);
+            this.form2.Controls.Add(this.lblTongsl);
+            this.form2.Controls.Add(this.txtSl);
+            this.form2.Controls.Add(this.txtTb);
+            this.form2.Controls.Add(this.btnXemthongtin);
+            this.form2.Controls.Add(this.txtTongtien);
+            this.form2.Location = new System.Drawing.Point(0, 220);
+            this.form2.Name = "form2";
+            this.form2.Size = new System.Drawing.Size(800, 233);
+            this.form2.TabIndex = 1;
+            this.form2.TabStop = false;
+            this.form2.Text = "Thong ke";
+            // 
+            // lblTrungbinhLanban
+            // 
+            this.lblTrungbinhLanban.Location = new System.Drawing.Point(61, 147);
+            this.lblTrungbinhLanban.Name = "lblTrungbinhLanban";
+            this.lblTrungbinhLanban.Size = new System.Drawing.Size(188, 36);
+            this.lblTrungbinhLanban.TabIndex = 1;
+            this.lblTrungbinhLanban.Text = "Trung binh/Lan ban";
+            // 
+            // lblTongtien
+            // 
+            this.lblTongtien.Location = new System.Drawing.Point(61, 91);
+            this.lblTongtien.Name = "lblTongtien";
+            this.lblTongtien.Size = new System.Drawing.Size(88, 38);
+            this.lblTongtien.TabIndex = 2;
+            this.lblTongtien.Text = "Tong tien";
+            // 
+            // lblTongsl
+            // 
+            this.lblTongsl.Location = new System.Drawing.Point(61, 42);
+            this.lblTongsl.Name = "lblTongsl";
+            this.lblTongsl.Size = new System.Drawing.Size(134, 38);
+            this.lblTongsl.TabIndex = 3;
+            this.lblTongsl.Text = "Tong so luong";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // txtThanhtien
+            // 
+            this.txtThanhtien.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txtThanhtien.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtThanhtien.Location = new System.Drawing.Point(161, 128);
+            this.txtThanhtien.Name = "txtThanhtien";
+            this.txtThanhtien.Size = new System.Drawing.Size(234, 36);
+            this.txtThanhtien.TabIndex = 2;
+            // 
+            // txtTongtien
+            // 
+            this.txtTongtien.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txtTongtien.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTongtien.Location = new System.Drawing.Point(250, 91);
+            this.txtTongtien.Name = "txtTongtien";
+            this.txtTongtien.Size = new System.Drawing.Size(234, 36);
+            this.txtTongtien.TabIndex = 2;
+            // 
+            // btnBanmoi
+            // 
+            this.btnBanmoi.Location = new System.Drawing.Point(86, 176);
+            this.btnBanmoi.Name = "btnBanmoi";
+            this.btnBanmoi.Size = new System.Drawing.Size(112, 34);
+            this.btnBanmoi.TabIndex = 3;
+            this.btnBanmoi.Text = "Ban Moi";
+            this.btnBanmoi.UseVisualStyleBackColor = true;
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Location = new System.Drawing.Point(270, 176);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(112, 34);
+            this.btnTinh.TabIndex = 3;
+            this.btnTinh.Text = "Tinh";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(565, 180);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(112, 34);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoat";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // radCfda
+            // 
+            this.radCfda.AutoSize = true;
+            this.radCfda.ForeColor = System.Drawing.Color.Fuchsia;
+            this.radCfda.Location = new System.Drawing.Point(565, 21);
+            this.radCfda.Name = "radCfda";
+            this.radCfda.Size = new System.Drawing.Size(142, 29);
+            this.radCfda.TabIndex = 4;
+            this.radCfda.TabStop = true;
+            this.radCfda.Text = "Cafe da 7000";
+            this.radCfda.UseVisualStyleBackColor = true;
+            // 
+            // radCfsua
+            // 
+            this.radCfsua.AutoSize = true;
+            this.radCfsua.ForeColor = System.Drawing.Color.Fuchsia;
+            this.radCfsua.Location = new System.Drawing.Point(565, 47);
+            this.radCfsua.Name = "radCfsua";
+            this.radCfsua.Size = new System.Drawing.Size(149, 29);
+            this.radCfsua.TabIndex = 4;
+            this.radCfsua.TabStop = true;
+            this.radCfsua.Text = "Cafe sua 8000";
+            this.radCfsua.UseVisualStyleBackColor = true;
+            this.radCfsua.CheckedChanged += new System.EventHandler(this.radCfsua_CheckedChanged);
+            // 
+            // radBia
+            // 
+            this.radBia.AutoSize = true;
+            this.radBia.ForeColor = System.Drawing.Color.Fuchsia;
+            this.radBia.Location = new System.Drawing.Point(565, 79);
+            this.radBia.Name = "radBia";
+            this.radBia.Size = new System.Drawing.Size(115, 29);
+            this.radBia.TabIndex = 4;
+            this.radBia.TabStop = true;
+            this.radBia.Text = "Bia 11000";
+            this.radBia.UseVisualStyleBackColor = true;
+            // 
+            // radRebbull
+            // 
+            this.radRebbull.AutoSize = true;
+            this.radRebbull.ForeColor = System.Drawing.Color.Fuchsia;
+            this.radRebbull.Location = new System.Drawing.Point(565, 105);
+            this.radRebbull.Name = "radRebbull";
+            this.radRebbull.Size = new System.Drawing.Size(140, 29);
+            this.radRebbull.TabIndex = 4;
+            this.radRebbull.TabStop = true;
+            this.radRebbull.Text = "RedBull 8000";
+            this.radRebbull.UseVisualStyleBackColor = true;
+            this.radRebbull.CheckedChanged += new System.EventHandler(this.radRebbull_CheckedChanged);
+            // 
+            // ckbMuave
+            // 
+            this.ckbMuave.AutoSize = true;
+            this.ckbMuave.ForeColor = System.Drawing.Color.Fuchsia;
+            this.ckbMuave.Location = new System.Drawing.Point(565, 140);
+            this.ckbMuave.Name = "ckbMuave";
+            this.ckbMuave.Size = new System.Drawing.Size(126, 29);
+            this.ckbMuave.TabIndex = 5;
+            this.ckbMuave.Text = "Mua ve 5%";
+            this.ckbMuave.UseVisualStyleBackColor = true;
+            // 
+            // txtTb
+            // 
+            this.txtTb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txtTb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTb.Location = new System.Drawing.Point(250, 147);
+            this.txtTb.Name = "txtTb";
+            this.txtTb.Size = new System.Drawing.Size(234, 36);
+            this.txtTb.TabIndex = 2;
+            // 
+            // txtSl
+            // 
+            this.txtSl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txtSl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSl.Location = new System.Drawing.Point(250, 42);
+            this.txtSl.Name = "txtSl";
+            this.txtSl.Size = new System.Drawing.Size(234, 36);
+            this.txtSl.TabIndex = 2;
+            // 
+            // txtHoadon
+            // 
+            this.txtHoadon.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txtHoadon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtHoadon.Location = new System.Drawing.Point(161, 72);
+            this.txtHoadon.Name = "txtHoadon";
+            this.txtHoadon.Size = new System.Drawing.Size(234, 36);
+            this.txtHoadon.TabIndex = 2;
+            // 
+            // btnXemthongtin
+            // 
+            this.btnXemthongtin.Location = new System.Drawing.Point(359, 193);
+            this.btnXemthongtin.Name = "btnXemthongtin";
+            this.btnXemthongtin.Size = new System.Drawing.Size(112, 34);
+            this.btnXemthongtin.TabIndex = 3;
+            this.btnXemthongtin.Text = "Xem Thong Tin";
+            this.btnXemthongtin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.form2);
+            this.Controls.Add(this.grb1);
+            this.Name = "Form1";
+            this.Text = "cua hang ban nuoc giai khat";
+            this.grb1.ResumeLayout(false);
+            this.grb1.PerformLayout();
+            this.form2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grb1;
+        private System.Windows.Forms.Label lblThanhtien;
+        private System.Windows.Forms.Label lblHoadon;
+        private System.Windows.Forms.Label lblSoluong;
+        private System.Windows.Forms.GroupBox form2;
+        private System.Windows.Forms.Label lblTrungbinhLanban;
+        private System.Windows.Forms.Label lblTongtien;
+        private System.Windows.Forms.Label lblTongsl;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.Button btnBanmoi;
+        private System.Windows.Forms.Label txtTongtien;
+        private System.Windows.Forms.Label txtThanhtien;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox ckbMuave;
+        private System.Windows.Forms.RadioButton radRebbull;
+        private System.Windows.Forms.RadioButton radBia;
+        private System.Windows.Forms.RadioButton radCfsua;
+        private System.Windows.Forms.RadioButton radCfda;
+        private System.Windows.Forms.Label txtSl;
+        private System.Windows.Forms.Label txtTb;
+        private System.Windows.Forms.Label txtHoadon;
+        private System.Windows.Forms.Button btnXemthongtin;
+    }
+}
+
